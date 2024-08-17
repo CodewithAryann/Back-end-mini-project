00a98e3f725b211e4f112fe6bea999563af4ae9e
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-08-17 14:12:40 +0400

    17

---
+++ models/user.js
@@ -0,0 +1,11 @@
+const mongosse = require("mongoose");
+
+mongosse.connect("mongodb://127.0.01:27017/mini-backend-project");
+
+const Schema = mongosse.Schema ({
+    username: String,
+    name: String,
+    email: String,
+    password: String,
+    age: Number
+})
