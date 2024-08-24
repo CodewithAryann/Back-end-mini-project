cca962ff16e7da06678bbd292c0f59a30ed0a7c2
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-08-24 14:35:40 +0400

    24

---
+++ models/usermodel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27.017/Back-end-mini-project");
+
+const userSchema = mongoose.Schema({
+    FUllname: String,
+    email: String,
+    password: String,
+    cart : {
+        type: Array,
+        default: []
+    },
+    order : {
+        type: Array,
+        default: []
+    },
+    isAdmin : Boolean,
+    contact : Number,
+    picture: String
+}) 
