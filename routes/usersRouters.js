acf0e79de63c366ed71cee9247cad7835bd61488
Aryan <151847516+CodewithAryann@users.noreply.github.com>
2024-08-25 15:57:54 +0400

    24

--- routes/usersRouters.js
+++ routes/usersRouters.js
@@ -1,9 +1,20 @@
 const express = require("express");
+const usermodel = require("../models/usermodel");
 const routes = express.Router();
 
 routes.get("/", function( res, req){
     res.send("Hello World");
 })
+
+routes.post("/register", async function( res, req){
+    let { email, password, fullname} = res.body;
+    await usermodel.create({
+        email,
+        password,
+        fullname
+    });
+})
+
 
 
 module.exports = routes;
